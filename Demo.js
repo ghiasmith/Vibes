0cc4e7ccb137d0b3b38e906df98e067bf5151972
Ghia Smith <ghiaaa.smith@gmail.com>
2019-05-01 23:48:55 -0400

    files added

---
+++ Demo.js
@@ -0,0 +1,41 @@
+
+//statements are a command and statements are seperated by semicolons
+
+/*(also, this is a comment)
+this is a multi line comment
+
+var num= 16; //number
+var name= "Ghia"; //string
+var flag= false; //boolean
+
+
+*/
+
+/*document.write(num)
+num="Ghia"
+alert(num)
+*/
+//var a = 3;
+//var b = 2;
+//var result = a+b;
+//document.write("<h1>Result is: "+result+"</h1>")
+
+//even or odd
+//pos neg or zero
+//pos and even
+/*
+var x = 6;
+if(x>0)
+	{
+		document write("<h1>Positive</h1>");
+	}
+	else if(x<0)
+	{
+		document.write("<h1>Negative</h1>")
+	}
+	else
+	{
+		document.write("<h1>Number is neither positive or negative</h1>")
+	}
+	*/
+	
